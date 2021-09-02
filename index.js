25332333462b767c127e7ab03dd7705b8fb2c6d3
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-02 16:54:32 -0700

    ready to add .env

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const server = require("express")();
+const express = require("express");
+const cors = require("cors");
+const server = express();
 
-server.listen(5000, () => {
-  console.log("listening on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("<h1>Module Project 4</h1>");
+});
+
+server.get("/api", (req, res) => {
+  res.json({ message: "hey, this thing works!" });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
