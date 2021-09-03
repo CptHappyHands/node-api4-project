2b2a7850236efaed74700a93cf87034b6ba9fa16
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-02 17:30:07 -0700

    endpoints finished

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(cors());
-server.use("/api/users", usersRouter);
+server.use("/api", usersRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1>Module Project 4</h1>");
--- users/users-routers.js
+++ users/users-routers.js
@@ -14,8 +14,26 @@ const users = [
   { name: "Aragorn" },
 ];
 
-router.get("/api/users", (req, res) => {
+router.get("/users", (req, res) => {
   res.status(200).json(users);
+});
+
+router.post("/register", (req, res) => {
+  const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({ message: "name required" });
+  } else {
+    return (req.name = name.trim());
+  }
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({ message: "username and password required" });
+  } else {
+    return "Welcome traveller, to the Inn of the Prancing Pony";
+  }
 });
 
 module.exports = router;
