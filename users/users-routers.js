9297cef3d90080f23ceb129da41cc3e1145c22b0
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-02 17:10:57 -0700

    testing users

---
+++ users/users-routers.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+const users = [
+  { name: "Frodo Baggins" },
+  { name: "Samwise Gamgee" },
+  { name: "Meriadoc Brandybuck" },
+  { name: "Peregrin Took" },
+  { name: "Mithrandir" },
+  { name: "Boromir" },
+  { name: "Legolas" },
+  { name: "Gimli" },
+  { name: "Aragorn" },
+];
+
+router.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
