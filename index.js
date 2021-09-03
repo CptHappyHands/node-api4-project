09e43690664ce166b7ef94fb702f8a7da5175399
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-02 17:16:25 -0700

    added module.exports

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const server = express();
+const usersRouter = require("./users/users-routers");
 
 const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(cors());
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1>Module Project 4</h1>");
--- users/users-routers.js
+++ users/users-routers.js
@@ -17,3 +17,5 @@ const users = [
 router.get("/api/users", (req, res) => {
   res.status(200).json(users);
 });
+
+module.exports = router;
